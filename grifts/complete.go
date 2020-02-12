16f6542c44fe0304f48cfe4a05aadf1c793a74a1
Samiul Hoque <samiul@monarko.com>
2020-02-12 17:57:36 +0600

    Update diagnose-complete event handling

--- grifts/complete.go
+++ grifts/complete.go
@@ -26,66 +26,121 @@ var _ = Namespace("pubsub", func() {
                 return fmt.Errorf("image-diagnose: %v", err)
             }
 
-            fullTopic := envy.Get("IMAGE_DIAGNOSE_SUB", "")
+            fullTopic := envy.Get("IMAGE_DIAGNOSE_COMPLETE_SUB", "")
             br := strings.SplitN(fullTopic, "/", -1)
             projectID := br[1]
             subID := br[3]
             msgs, err := helpers.PubSubPullMessages(projectID, subID)
             if err != nil {
-                return fmt.Errorf("image-diagnose-sub: %v", err)
+                return fmt.Errorf("image-diagnose-complete-sub: %v", err)
+            }
+
+            drOptions := []string{
+                "Ungradeable",
+                "Normal",
+                "Mild DR",
+                "Moderate DR",
+                "Severe DR",
+                "Proliferative DR",
+            }
+            dmeOptions := map[string]string{
+                "yes": "Present",
+                "no":  "Not Present",
             }
 
             for _, m := range msgs {
                 p := map[string]interface{}{}
                 err := json.Unmarshal(m, &p)
                 if err != nil {
-                    log.Printf("image-diagnose unmarshal: %v", err)
+                    log.Printf("image-diagnose-complete unmarshal: %v", err)
                     continue
                 }
-                images := p["images"].([]map[string]interface{})
+                referral := p["referral"].(string)
+                diagnosis := p["diagnosis"].([]map[string]interface{})
 
-                for _, i := range images {
+                for _, d := range diagnosis {
                     screening := &models.Screening{}
-                    q := tx.Where("accession_id = ?", i["accession_number"])
+                    q := tx.Where("accession_id = ?", d["accession_number"])
                     err = q.First(screening)
                     if err != nil {
-                        log.Printf("image-diagnose find screening: %v", err)
+                        log.Printf("image-diagnose-complete find screening: %v", err)
                         continue
                     }
 
                     scrImage := &models.ScreeningImage{}
-                    q = tx.Where("data->>'sop_instance_uid' = ?", i["sop_instance_uid"])
+                    q = tx.Where("data->>'sop_instance_uid' = ?", d["sop_instance_uid"])
                     err = q.First(scrImage)
                     if err != nil {
-                        log.Printf("image-diagnose find scr image: %v", err)
+                        log.Printf("image-diagnose-complete find scr image: %v", err)
                         continue
                     }
                     data := scrImage.Data
-                    data["is_success"] = i["is_success"]
-                    if r, ok := i["reason"]; ok {
-                        data["reason"] = r
-                    }
+                    laterality := data["laterality"]
+                    data["dr_grade"] = d["dr_grade"]
+                    data["dme_grade"] = d["dme_grade"]
                     scrImage.Data = data
 
                     verrs, err := tx.ValidateAndUpdate(scrImage)
                     if err != nil {
-                        log.Printf("image-diagnose update image: %v", err)
+                        log.Printf("image-diagnose-complete update image: %v", err)
                         continue
                     }
                     if verrs.HasAny() {
-                        log.Printf("image-diagnose screening image: %v", verrs.Errors)
+                        log.Printf("image-diagnose-complete screening image: %v", verrs.Errors)
                         continue
                     }
 
                     if screening.HubStatus.String == "diagnosing" {
                         screening.HubStatus.String = "diagnosed"
+                        dr := d["dr_grade"].(map[string]interface{})
+                        dme := d["dme_grade"].(map[string]interface{})
+                        if laterality == "R" {
+                            drGrade := dr["grade"].(string)
+                            m, l := helpers.PartiallyMatch(drOptions, drGrade, 4)
+                            if l >= 0 {
+                                screening.Eyes.RightEye.DRGrading.String = m
+                                screening.Eyes.RightEye.DRGrading.Valid = true
+                            }
+
+                            dmeGrade := dme["grade"].(string)
+                            dm := "Ungradeable"
+                            if !strings.HasPrefix(strings.ToLower(dmeGrade), "un") {
+                                dm = dmeOptions[strings.ToLower(dmeGrade)]
+                            }
+                            screening.Eyes.RightEye.DMEAssessment.String = dm
+                            screening.Eyes.RightEye.DMEAssessment.Valid = true
+                        } else {
+                            drGrade := dr["grade"].(string)
+                            m, l := helpers.PartiallyMatch(drOptions, drGrade, 4)
+                            if l >= 0 {
+                                screening.Eyes.LeftEye.DRGrading.String = m
+                                screening.Eyes.LeftEye.DRGrading.Valid = true
+                            }
+
+                            dmeGrade := dme["grade"].(string)
+                            dm := "Ungradeable"
+                            if !strings.HasPrefix(strings.ToLower(dmeGrade), "un") {
+                                dm = dmeOptions[strings.ToLower(dmeGrade)]
+                            }
+                            screening.Eyes.LeftEye.DMEAssessment.String = dm
+                            screening.Eyes.LeftEye.DMEAssessment.Valid = true
+                        }
+                        screening.Referral.Referred.Valid = true
+                        if strings.HasPrefix(strings.ToLower(referral), "refer") {
+                            screening.Referral.Referred.Bool = true
+                        } else {
+                            screening.Referral.Referred.Bool = false
+                        }
+                        screening.Referral.Notes.String = referral
+                        screening.Referral.Notes.Valid = true
+
                         verrs, err = tx.ValidateAndUpdate(screening)
                         if err != nil {
-                            log.Printf("image-diagnose screening update: %v", err)
+                            log.Printf("image-diagnose-complete screening update: %v", err)
                             continue
                         }
                         if verrs.HasAny() {
-                            log.Printf("image-diagnose screening: %v", verrs.Errors)
+                            log.Printf("image-diagnose-complete screening: %v", verrs.Errors)
                             continue
                         }
                     }
